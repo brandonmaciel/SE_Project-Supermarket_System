9002dc0821c0e6492820af9f62cd93b4e89b79c2
brandonmaciel <brandonmaciel4@gmail.com>
2021-11-23 22:32:39 -0600

    refactored

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.example.se_projectsupermarket_system {
     requires javafx.controls;
     requires javafx.fxml;
+    requires com.google.gson;
 
 
     opens com.example.se_projectsupermarket_system to javafx.fxml;
